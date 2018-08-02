70efcca2ed846f0591338479dafd9e089ae9cc5e
AE206_PC4 <AE206_PC4@Infva07294>
2018-08-02 10:02:01 +0530

    first commiy

---
+++ OOPs/src/Member1.java
@@ -0,0 +1,16 @@
+
+public class Member1 {
+
+	private String name;
+	public Member1() {
+		// TODO Auto-generated constructor stub
+	}
+	public Member1(String name)
+	{
+		this.name=name;
+	}
+	public String getMember1() {
+		return name;
+	}	
+	
+}
